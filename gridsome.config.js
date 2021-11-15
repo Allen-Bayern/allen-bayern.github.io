c5784cbb0e68e099f73345acda9d9b0d14f27465
allen-byn <nobodyplay@yeah.net>
2021-11-15 13:40:52 +0800

    bundle to github

---
+++ gridsome.config.js
@@ -0,0 +1,12 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: '月饼的技术博客',
+  plugins: [],
+  siteUrl : 'https://allen-bayern.github.io',
+  pathPrefix: '/yuebing-blog',
+};
