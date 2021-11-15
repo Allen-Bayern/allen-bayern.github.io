cd7d9315d24d69783bba22ba5f5c18a7b9a7c037
allen-byn <nobodyplay@yeah.net>
2021-11-15 14:04:16 +0800

    Rewrite index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,7 +7,7 @@
     <h1>Hello, world!</h1>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      你们好，我叫月饼，他们都叫我饼哥！
     </p>
 
     <p class="home-links">
