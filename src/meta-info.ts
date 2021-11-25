8db32a34de1ceb21ea1a770b5af46e70a557e226
allen <nobodyplay@yeah.net>
2021-11-25 18:04:55 +0800

    .d.ts是真的好用

--- src/meta-info.ts
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-
-declare module "vue/types/options" {
-  export interface ComponentOptions<V extends Vue> {
-      metaInfo?: any;
-  }
-
-  // export default ComponentOptions;
-}
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,12 @@
 <template>
   <Layout>
-    你已经为他续了{{ N }}秒！请继续加油
+    你已经为他续了{{ N }}秒！请继续加油!
     <button @click="add">+1s</button>
   </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-// import "~/meta-info";
 
 let N : number;
 
